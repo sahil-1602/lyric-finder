32ff960a696927f822fec25619af7a10c222d17b
sahil-1602 <sahildarji16@gmail.com>
2021-02-16 15:36:42 +0530

    styled components

--- src/components/Lyric.jsx
+++ src/components/Lyric.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react';
 import {LyricContext} from '../contexts/Lyric.context';
+import '../sass/_component.scss';
 
 export default function Navbar(props) {
-    const {lyrics} = useContext(LyricContext)
+    const {lyrics} = useContext(LyricContext);
+    console.log(lyrics);
     return (
-        <div>
-            <h1>{lyrics.lyrics_body}</h1>
+        <div className="lyric-container">
+            <div className="lyric-container--name">
+                <h2>{lyrics[1].song}</h2>
+                <h2>{lyrics[0].explicit == 1 ? "Explicit" : ""}</h2>
+            </div>
+            <h1>{lyrics[0].lyrics_body}</h1>
         </div>
     )
 }
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -6,6 +6,7 @@ import {LyricContext} from '../contexts/Lyric.context';
 
 export default function Track(props) {
 
+    let song = props.track
     const history = useHistory();
     const { updateLyric } = useContext(LyricContext);
 
@@ -14,22 +15,29 @@ export default function Track(props) {
         axios.get(MM_LYRIC_API)
         .then((res) => {
             const lyric = res.data.message.body.lyrics;
-            updateLyric(lyric);
+            const track = {
+                song: song.track_name,
+            }
+            const lyricsWithTrackName = [lyric, track];
+            updateLyric(lyricsWithTrackName);
             history.push('/lyrics');
         })
         .catch(err => console.log(err));
     };
-
-    let song = props.track
     
     return (
         <div className="card">
-            <div className="card--song"><p>{song.artist_name}</p></div>
-            <div className="card--part2">
-                <div className="card--track"><p>Track : {song.track_name}</p></div>
-                <div className="card--album"><p>Album: {song.album_name}</p></div>
+            <div className="card--img">
+                <img src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/06/music-logo-design.jpg" />
+            </div>
+            <div className="card--details">
+                <div className="card--track"><h4>{song.track_name}</h4></div>
+                <div className="card--part2">
+                <div className="card--artist"><p><span>Artist</span> : {song.artist_name}</p></div>
+                <div className="card--artist"><p><span>Album</span> : {song.album_name}</p></div>
                 <div className="card--link"><button onClick={() => handleClick()}>View Lyrics</button></div>
-            </div>    
+                </div> 
+            </div>   
         </div>
     )
 }
